a547cc2b4cbfb9298a9328b39689c4c1638c071d
Thicodes <thicodes@gmail.com>
2025-02-14 16:31:47 -0300

    Rotas por meio de TagHelper

--- src/AppSemTemplate/Extensions/BotaoTagHelper.cs
+++ src/AppSemTemplate/Extensions/BotaoTagHelper.cs
@@ -6,11 +6,14 @@ namespace AppSemTemplate.Extensions
     [HtmlTargetElement("*", Attributes = "tipo-botao, route-id")]
     public class BotaoTagHelper : TagHelper
     {
-        private readonly IHttpContextAccessor _contextAccessor;
+        private readonly IHttpContextAccessor _contextAccessor; 
 
-        public BotaoTagHelper(IHttpContextAccessor contextAccessor)
+        private readonly LinkGenerator _linkGenerator;
+
+        public BotaoTagHelper(IHttpContextAccessor contextAccessor, LinkGenerator linkGenerator)
         {
             _contextAccessor = contextAccessor;
+            _linkGenerator = linkGenerator;
         }
 
         [HtmlAttributeName("tipo-botao")]
@@ -46,8 +49,17 @@ namespace AppSemTemplate.Extensions
 
             var controller = _contextAccessor.HttpContext?.GetRouteData().Values["controller"]?.ToString();
 
+            var host = $"{_contextAccessor.HttpContext.Request.Scheme}://" + $"{_contextAccessor.HttpContext.Request.Host.Value}";
+
+            var indexPath = _linkGenerator.GetPathByAction(
+                _contextAccessor.HttpContext,
+                nomeAction,
+                controller,
+                values: new {id = RouteId}
+            )!;
+
             output.TagName = "a";
-            output.Attributes.SetAttribute("href", $"{controller}/{nomeAction}/{RouteId}");
+            output.Attributes.SetAttribute("href", $"{indexPath}");
             output.Attributes.SetAttribute("class", nomeClasse);
 
             var iconSpan = new TagBuilder("span");
