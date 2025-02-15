0b7ff5f138c66237f057f0b088e79bffa32d5158
Thicodes <thicodes@gmail.com>
2025-02-14 22:23:44 -0300

    Rota em atributo Teste Pedido

---
+++ src/AppSemTemplate/Modulos/Produtos/Controllers/CadastroController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppSemTemplate.Areas.Produtos.Controllers
+{
+    [Area("Produtos")]
+    public class CadastroController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- src/AppSemTemplate/Program.cs
+++ src/AppSemTemplate/Program.cs
@@ -1,10 +1,20 @@
 using AppSemTemplate.Data;
 using Microsoft.EntityFrameworkCore;
 using AppSemTemplate.Extensions;
+using Microsoft.AspNetCore.Mvc.Razor;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
+
+
+builder.Services.Configure<RazorViewEngineOptions>(options =>
+{
+    options.AreaViewLocationFormats.Clear();
+    options.AreaViewLocationFormats.Add("/Modulos/{2}/Views/{1}/{0}.cshtml");
+    options.AreaViewLocationFormats.Add("/Modulos/{2}/Views/Shared/{0}.cshtml");
+    options.AreaViewLocationFormats.Add("/Views/Shared/{0}.cshtml");
+});
 
 //builder.Services.AddRouting(options =>
 //{
@@ -28,9 +38,12 @@ app.UseRouting();
   //  name: "default",
     //pattern: "{controller:slugify=Home}/{action=sluify=}/{id?}");
 
-app.MapControllerRoute(
-    name : "areas",
-    pattern : "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+//app.MapControllerRoute(
+  //  name : "areas",
+    //pattern : "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+    app.MapAreaControllerRoute("AreaProdutos", "Produtos", "Produto/{controller=Cadastro}/{action=Index}/{id?}");
+    app.MapAreaControllerRoute("AreaVendas", "Vendas", "Vendas/{controller=Gestao}/{action=Index}/{id?}");
 
 app.MapControllerRoute(
     name: "default",
